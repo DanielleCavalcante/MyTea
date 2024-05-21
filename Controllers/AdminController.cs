1416e100cb24b50bf581eaf6b956733852ed4b45
natalia-r-l <resina.natalia@gmail.com>
2024-05-21 14:18:00 -0300

    estruturacao em pastas e criação de pagina wbs

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTea.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Admin()
+        {
+            return View();
+        }
+      
+    }
+}

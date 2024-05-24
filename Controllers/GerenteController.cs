d9edf45fd648462b86d2e003cd6d2650b8380f7a
natalia-r-l <resina.natalia@gmail.com>
2024-05-24 16:55:02 -0300

    criação da view e do controller gerente e mudança da nabar do layout para uma partial view

---
+++ Controllers/GerenteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTea.Controllers
+{
+    public class GerenteController : Controller
+    {
+        public IActionResult Gerente()
+        {
+            return View();
+        }
+    }
+}

c0ef0850c7b8c13f617df9e7eeb0d36a3fee9de6
Tatiana Gissi Fonseca <tatiana.gissi@gmail.com>
2024-05-24 02:49:43 -0300

    adicionados Models WBS, Departamento e Funcionario

---
+++ Models/WBS.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTea.Models
+{
+    public class WBS
+    {
+        public int WBS_Id { get; set; }
+        [Required]
+        public string? WBS_Codigo { get; set; }
+        [Required]
+        public string? WBS_Descricao {  get; set; }
+        [Required]
+        public bool WBS_Chargeability {  get; set; }
+
+    }
+}

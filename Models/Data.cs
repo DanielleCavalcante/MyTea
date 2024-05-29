c56ced151b7db20990e56a095ece23a4ada95abe
Tatiana Gissi Fonseca <tatiana.gissi@gmail.com>
2024-05-29 14:07:49 -0300

    altera tipo datetime para dateonly

--- Models/Data.cs
+++ Models/Data.cs
@@ -4,16 +4,16 @@ namespace MyTea.Models
 {
     public class Data
     {
-        public DateTime Dt_Id { get; set; }
+        public DateOnly Dt_Id { get; set; }
         public int Dt_Ano { get; set; }
         public int Dt_Mes { get; set; }
         public int Dt_Dia { get; set; }
         public string? Dt_Mes_Nome { get; set; }
-        public DateTime Dt_Mes_DtInicial { get; set; }
-        public DateTime Dt_Mes_DtFinal { get; set; }
+        public DateOnly Dt_Mes_DtInicial { get; set; }
+        public DateOnly Dt_Mes_DtFinal { get; set; }
         public int Dt_Quinzena_Mes { get; set; }
-        public DateTime Dt_Quinzena_DtInicial { get; set; }
-        public DateTime Dt_Quinzena_DtFinal { get; set; }
+        public DateOnly Dt_Quinzena_DtInicial { get; set; }
+        public DateOnly Dt_Quinzena_DtFinal { get; set; }
         public int Dt_DiaSemana { get; set; }
         public string? Dt_DiaSemana_Nome { get; set; }
         public bool Dt_Feriado { get; set; }

76aa4f95fde36a0feec351dbb913c0085ea2f9f5
natalia-r-l <resina.natalia@gmail.com>
2024-05-29 03:02:22 -0300

    mudança de estrutura de partial views para paginas independentes

---
+++ Controllers/WBSController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTea.Controllers
+{
+    public class WBSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

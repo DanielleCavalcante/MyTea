05693e383d7acae1eb29cb9b1d061e4adbcbd2fb
DanielleCavalcante <daniellesoucavalcante1324@gmail.com>
2024-06-05 08:02:12 -0300

    viewmodel lancamento horas

--- Models/ViewModelListar.cs
+++ Models/ViewModelListar.cs
@@ -1,11 +1,21 @@
 ﻿namespace MyTea.Models
 {
-    //model para listar departamento e exibir no select
     public class ViewModelListar
     {
+        // Lista de datas
+        public List<Data> Datas { get; set; }
+
+        // Funcionario atual
         public Funcionario Funcionario { get; set; }
-        public List<Funcionario> listarFunc { get; set; }
-        public List<Departamento> listarDepto { get; set; }
+
+        // Lista de Work Breakdown Structures
+        public List<WBS> WBSs { get; set; } 
+
+        // Lista de departamentos
+        public List<Departamento> Departamentos { get; set; }
+
+        // Lista de níveis de acesso
         public List<NivelAcesso> NiveisAcesso { get; set; }
     }
 }
+

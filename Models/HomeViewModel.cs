7ec933c8ba236341a8d9c510fc682eb6ab7c7cfa
DanielleCavalcante <daniellesoucavalcante1324@gmail.com>
2024-06-05 08:21:24 -0300

    mudando nome da viewmodel

---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MyTea.Models
+{
+    public class HomeViewModel
+    {
+        // Lista de datas
+        public List<Data> Datas { get; set; }
+
+        // Funcionario atual
+        public Funcionario Funcionario { get; set; }
+
+        // Lista de Work Breakdown Structures
+        public List<WBS> WBSs { get; set; } 
+
+        // Lista de departamentos
+        public List<Departamento> Departamentos { get; set; }
+
+        // Lista de níveis de acesso
+        public List<NivelAcesso> NiveisAcesso { get; set; }
+    }
+}
+
